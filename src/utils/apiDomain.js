c7f50642dc2bc00a338a849c8ae12760f6ecf0c7
Abdifatah Mohamed <mofaatah@gmail.com>
2022-09-17 00:21:33 -0500

    deleted all files

--- src/utils/apiDomain.js
+++
@@ -1,6 +0,0 @@
-export function apiDomain() {
-  const production = process.env.NODE_ENV === "production";
-  return production
-    ? "https://it-logger-jsonserver.herokuapp.com"
-    : "http://localhost:5000";
-}
