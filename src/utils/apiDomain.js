9df12b640f54bc0de1713678da294e24ccba48c3
Abdifatah Mohamed <mofaatah@gmail.com>
2022-09-17 18:26:39 -0500

    Designed fake API for my backend

--- src/utils/apiDomain.js
+++ src/utils/apiDomain.js
@@ -1,10 +1,10 @@
 export function apiDomain() {
   const production = process.env.NODE_ENV === "production";
   return production
-    ? "https://it-logger-jsonserver.herokuapp.com"
+    ? "https://dct-issue-tracker.herokuapp.com"
     : "http://localhost:5000";
 }
 
 
-// https://dct-issue-tracker.herokuapp.com/
+// https://dct-issue-tracker.herokuapp.com
 // https://it-logger-jsonserver.herokuapp.com
