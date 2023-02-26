b503cf39bb175f501d3e4e8a2f856a06e703c326
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-02-26 17:15:51 -0600

    Update apiDomain.js

--- src/utils/apiDomain.js
+++ src/utils/apiDomain.js
@@ -1,10 +1,11 @@
 export function apiDomain() {
   const production = process.env.NODE_ENV === "production";
   return production
-    ? "https://dct-issue-tracker.herokuapp.com"
+    ? "https://dct-api.onrender.com/"
     : "http://localhost:5000";
 }
 
 
 // https://dct-issue-tracker.herokuapp.com
 // https://it-logger-jsonserver.herokuapp.com
+// https://dct-issue-tracker.herokuapp.com
