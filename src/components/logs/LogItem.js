e0b341b7ff3819f939b4fc01633d15f998ea08ca
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-03-17 13:10:05 -0500

    Update LogItem.js

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Moment from "react-moment";
-import "moment/local/ct";
+import "moment/locale/ct";
 import { connect } from "react-redux";
 import { deleteLog, setCurrent } from "../../Actions/logActions";
 
