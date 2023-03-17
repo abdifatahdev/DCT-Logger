0fbafe25120e6a819bf62ca00dbd97f0be965fdc
Abdifatah Mohamed <48977326+abdifatahmohamad@users.noreply.github.com>
2023-03-17 12:55:31 -0500

    Update LogItem.js

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,40 +1,37 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Moment from "react-moment";
-import "moment/locale/es";
-import { connect } from "react-redux";
-import { deleteLog, setCurrent } from "../../Actions/logActions";
+import React from 'react';
+import Moment from 'react-moment';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { deleteLog, setCurrent } from '../../actions/logActions';
 
-import M from "materialize-css/dist/js/materialize.min.js";
+import M from 'materialize-css/dist/js/materialize.min.js';
+
 const LogItem = ({ log, deleteLog, setCurrent }) => {
-  const { message, attention, date, id, tech } = log;
-  const onDelete = (id) => {
-    deleteLog(id);
-    M.toast({ html: "Log Deleted" });
+  const onDelete = () => {
+    deleteLog(log.id);
+    M.toast({ html: 'Log Deleted' });
   };
+
   return (
-    <li className="collection-item">
+    <li className='collection-item'>
       <div>
         <a
-          href="#edit-log-modal"
-          className={`modal-trigger ${attention ? "red-text" : "blue-text"}`}
+          href='#edit-log-modal'
+          className={`modal-trigger ${
+            log.attention ? 'red-text' : 'blue-text'
+          }`}
           onClick={() => setCurrent(log)}
         >
-          {message}
+          {log.message}
         </a>
         <br />
-        <span className="grey-text">
-          <span className="black-text">ID #{id}</span> last updated by <span className="black-text">{tech}</span> on{" "}
-          <Moment format="DD MMMM  YYYY,h:mm:ss a" local="ct">
-             {date}
-          </Moment>
+        <span className='grey-text'>
+          <span className='black-text'>ID #{log.id}</span> last updated by{' '}
+          <span className='black-text'>{log.tech}</span> on{' '}
+          <Moment format='MMMM Do YYYY, h:mm:ss a'>{log.date}</Moment>
         </span>
-        <a
-          href="#!"
-          onClick={(e) => onDelete(id)}
-          className="secondary-content"
-        >
-          <i className="material-icons red-text">delete</i>
+        <a href='#!' onClick={onDelete} className='secondary-content'>
+          <i className='material-icons grey-text'>delete</i>
         </a>
       </div>
     </li>
@@ -44,7 +41,10 @@ const LogItem = ({ log, deleteLog, setCurrent }) => {
 LogItem.propTypes = {
   log: PropTypes.object.isRequired,
   deleteLog: PropTypes.func.isRequired,
-  setCurrent: PropTypes.func.isRequired,
+  setCurrent: PropTypes.func.isRequired
 };
 
-export default connect(null, { deleteLog, setCurrent })(LogItem);
+export default connect(
+  null,
+  { deleteLog, setCurrent }
+)(LogItem);
